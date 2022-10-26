7e834cf503addb1e93546a6fa06db7e524ac5d47
Slavyana Nedelcheva <slavy@hotmail.com>
2022-10-26 16:46:15 -0700

    Adding 2 js, 1 py exersice

---
+++ javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -0,0 +1,99 @@
+// @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./annalyns-infiltration.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Now help Annalyn free her best friend!
+
+/**
+ * The fast attack is available when the knight is sleeping
+ *
+ * @param {boolean} knightIsAwake
+ *
+ * @return {boolean} Whether or not you can execute a fast attack.
+ */
+export function canExecuteFastAttack(knightIsAwake) { 
+      return !knightIsAwake;  
+      //when the function needs to return the oposite of the parameter type
+      //then we have one line return statement no need for if statement 
+}
+
+/**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  // if (!knightIsAwake && !archerIsAwake && !prisonerIsAwake) {
+  //     return false;
+  // } else {
+  //     return true;
+  // };
+  return knightIsAwake || archerIsAwake || prisonerIsAwake;
+  //this is the oposite of the previus if statement
+  //so we don't need to use if statement
+}
+
+/**
+ * You'll get caught by the archer if you signal while they're awake.
+ *
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can send a signal to the prisoner.
+ */
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+    // if (!archerIsAwake && prisonerIsAwake) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    return !archerIsAwake && prisonerIsAwake; 
+    //only if this condition is met then the function return true 
+    //then we can have only return statement without if statement
+}
+
+/**
+ * The final stage in the plan: freeing Annalyn's best friend.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * @param {boolean} petDogIsPresent
+ *
+ * @returns {boolean} Whether or not you can free Annalyn's friend.
+ */
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+) {
+  // if (petDogIsPresent && !archerIsAwake) {
+  //   return true;
+  // } else if (prisonerIsAwake && !archerIsAwake && !knightIsAwake) {
+  //     return true;
+  // } else {
+  //     return false;
+  // }
+  return !archerIsAwake && (petDogIsPresent || prisonerIsAwake && !knightIsAwake);
+  //that is the oposite of the if statement 
+}
